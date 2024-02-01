1c7edede8971124998fae208f4008bc195370a4e
Jerome Lecoq <jlecoq@gmail.com>
2024-02-01 15:14:01 -0800

    Add basic math

--- delta2D_numcodecs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .delta2D import Delta2D
-
-
-from .version import version as __version__
--- poisson_numcodecs/Poisson.py
+++ poisson_numcodecs/Poisson.py
@@ -9,7 +9,6 @@ from numcodecs.compat import ndarray_copy
 
 
 ### NUMCODECS Codec ###
-
 class Poisson(Codec):
     """Codec for 3-dimensional Delta. The codec assumes that input data are of shape:
     (num_samples, num_channels).
@@ -28,39 +27,25 @@ class Poisson(Codec):
     def __init__(self, dark_signal, signal_to_photon_gain):
         self.dark_signal = dark_signal
         self.signal_to_photon_gain = signal_to_photon_gain
+        self.dtype = np.float32
 
     def encode(self, buf):
-        assert buf.ndim == 2, "Input data must be 2D"
-        assert buf.shape[1] == self.num_channels
-
         enc = np.zeros(buf.shape, dtype=self.dtype)
-
-        if self.axis == 0:
-            enc[0, :] = buf[0, :]
-            enc[1:] = np.diff(buf, axis=self.axis)
-        elif self.axis == 1:
-            enc[:, 0] = buf[:, 0]
-            enc[:, 1:] = np.diff(buf, axis=self.axis)
+        enc = np.sqrt((buf - self.dark_signal) / self.signal_to_photon_gain)
         return enc
 
     def decode(self, buf, out=None):
         buf = np.frombuffer(buf, self.dtype)
-        enc = buf.reshape(-1, self.num_channels)
-        
-        dec = np.empty_like(enc, dtype=self.dtype)
-        np.cumsum(enc, out=dec, axis=self.axis)
+        dec = buf ** 2 * self.signal_to_photon_gain + self.dark_signal
         out = ndarray_copy(dec, out)
-        
         return out
 
     def get_config(self):
         # override to handle encoding dtypes
         return dict(
             id=self.codec_id,
-            dtype=np.dtype(self.dtype).str if self.dtype is not None else None,
-            num_channels=self.num_channels,
-            axis=self.axis
+            dark_signal=self.dark_signal,
+            signal_to_photon_gain=self.signal_to_photon_gain
         )
-
 
 numcodecs.register_codec(Poisson)

fe51a827805e39e2279ab4f8090830e47155d57b
Jerome Lecoq <jlecoq@gmail.com>
2024-02-27 19:55:26 -0800

    Update test_poisson_codec.py

--- tests/test_poisson_codec.py
+++ tests/test_poisson_codec.py
@@ -22,10 +22,10 @@ def test_data(dtype="int16"):
     return [test2d, test2d_long]
 
 def test_poisson_encode_decode(test_data):
-    for integer_per_photon in [8, 9, 10]:
+    for beta in [0.5, 0.25, 0.1]:
         poisson_codec = Poisson(dark_signal=0, signal_to_photon_gain=1.0
                                 ,encoded_dtype='int16', decoded_dtype='int16',
-                                integer_per_photon=integer_per_photon)
+                                beta=beta)
         for example_data in test_data:
             encoded = poisson_codec.encode(example_data)
             decoded = poisson_codec.decode(encoded)

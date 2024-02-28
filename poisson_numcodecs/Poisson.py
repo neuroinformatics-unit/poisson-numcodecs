8ca9a01fb8dd9dd7c6a2eca6e8dfda31638bf63e
Jerome Lecoq <jlecoq@gmail.com>
2024-02-27 20:15:46 -0800

    Update Poisson.py

--- poisson_numcodecs/Poisson.py
+++ poisson_numcodecs/Poisson.py
@@ -44,7 +44,7 @@ class Poisson(Codec):
 
     def decode(self, buf, out=None):
         dec = ensure_ndarray(buf).view(self.encoded_dtype)
-        dec = ((dec.astype('float') * 2.0 / self.beta )**2 ) * self.signal_to_photon_gain + self.dark_signal
+        dec = ((dec.astype('float') * self.beta / 2.0 )**2 ) * self.signal_to_photon_gain + self.dark_signal
         outarray = np.round(dec)
         outarray = ndarray_copy(outarray, out)
         return outarray.astype(self.decoded_dtype)

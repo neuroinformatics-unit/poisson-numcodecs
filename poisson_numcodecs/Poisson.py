dccfdca529b3f2496a4206598ed7965d4dbcf496
Jerome Lecoq <jlecoq@gmail.com>
2024-02-27 22:32:05 -0800

    Stick to Anscombe
    Try to use the full unbiased backward form

--- poisson_numcodecs/Poisson.py
+++ poisson_numcodecs/Poisson.py
@@ -37,15 +37,34 @@ class Poisson(Codec):
 
     def encode(self, buf):
         enc = np.zeros(buf.shape, dtype=self.encoded_dtype)
+
+        # We convert to photons
         centered = (buf.astype('float') - self.dark_signal) / self.signal_to_photon_gain
-        enc = 2.0 / self.beta * (np.sqrt(np.maximum(0, centered)))
+
+        # https://en.wikipedia.org/wiki/Anscombe_transform for the forward
+        enc = 2.0 (np.sqrt(np.maximum(0, centered + 3/8)))
+
+        # This is the part that is applied to discard noise in compressed form
+        enc = enc / self.beta 
         enc = enc.astype(self.encoded_dtype)
+        
         return enc
 
     def decode(self, buf, out=None):
         dec = ensure_ndarray(buf).view(self.encoded_dtype)
-        dec = ((dec.astype('float') * self.beta / 2.0 )**2 ) * self.signal_to_photon_gain + self.dark_signal
+
+        # We first unapply beta
+        dec = dec.astype('float') * self.beta
+        
+        # https://en.wikipedia.org/wiki/Anscombe_transform for the inverse without bias
+        dec = dec**2 / 4.0 - 1/8 + 1/4*np.sqrt(1.5)/dec - 11/8/(dec**2)+5/8*np.sqrt(1.5)/(dec**3) 
+
+        # We convert back to arbitrary pixels
+        dec = dec * self.signal_to_photon_gain + self.dark_signal
+
+        # We have to go back to integers
         outarray = np.round(dec)
+        
         outarray = ndarray_copy(outarray, out)
         return outarray.astype(self.decoded_dtype)
 

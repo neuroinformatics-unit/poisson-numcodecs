a6547a2798269343b59dc6b71c6524c4f2b4f07a
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-12-06 18:05:14 +0000

    Update neuropil subtraction script

--- examples/convert_F_and_F_neu.py
+++ examples/convert_F_and_F_neu.py
@@ -44,14 +44,15 @@ def plot_f_fneu(f=None, fneu=None, f_subtracted=None, saving_path=None, title=No
     plt.plot(f, label='F')
     plt.plot(fneu, label='Fneu')
     plt.plot(f_subtracted, label='F - 0.7 * Fneu')
+
     plt.legend()
-    #  set figure size
     plt.gcf().set_size_inches(15, 5)
     plt.title(title)
+
     plt.savefig(saving_path)
     plt.close()
 
-def main(tif_path, suite_2p_path):
+def main(tif_path: Path, suite_2p_path: Path, make_plots: bool=False, roi_id: int=0):
 
     photon_sensitivity, dark_signal = get_adapted_parameters(tif_path)
     f_neu_path = Path(suite_2p_path) / 'plane0/Fneu.npy'
@@ -61,17 +62,19 @@ def main(tif_path, suite_2p_path):
     f_photon_flux, fneu_photon_flux = convert_f_fneu_to_photon_flux(f, fneu, photon_sensitivity, dark_signal)
     f_subtracted_flux = simple_neuropil_correction(f_photon_flux, fneu_photon_flux)
 
-    roi_id = 18
-    plot_f_fneu(f[roi_id], fneu[roi_id], f_subtracted[roi_id], 'figures/original_f_fneu.png', 'Original neuropil decontamination [a.u.]')
-    plot_f_fneu(f_photon_flux[roi_id], fneu_photon_flux[roi_id], f_subtracted_flux[roi_id],
-                'figures/photon_flux_f_fneu.png', 'Photon flux neuropil decontamination [photons/s]')
+    if make_plots:
+        plot_f_fneu(f[roi_id], fneu[roi_id], f_subtracted[roi_id], f'{tif_path.parent}/original_f_fneu_roi_{roi_id}.png', 'Original neuropil decontamination [a.u.]')
+        plot_f_fneu(f_photon_flux[roi_id], fneu_photon_flux[roi_id], f_subtracted_flux[roi_id],
+                    f'{tif_path.parent}/photon_flux_f_fneu_roi_{roi_id}.png', 'Photon flux neuropil decontamination [photons/pixel/frame]')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("tif_path", type=str, help="Path to the TIFF file")
-    parser.add_argument("suite_2p_path", type=str, help="Path to the Suite2P folder")
+    parser.add_argument("tif_path", type=Path, help="Path to the TIFF file")
+    parser.add_argument("suite_2p_path", type=Path, help="Path to the Suite2P folder")
+    parser.add_argument("--make_plots", help="Whether to make plots or not", default=False)
+    parser.add_argument("--roi_id", type=int, help="ROI id to plot", default=0)
 
     args = parser.parse_args()
 
-    main(args.tif_path, args.suite_2p_path)
+    main(args.tif_path, args.suite_2p_path, args.make_plots, args.roi_id)
